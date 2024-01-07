84ed685501bdf5d97e3588c6d0fa385ea92ed69b
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-08 00:10:25 +0500

    create user and contact models

---
+++ src/models/contact.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const contactSchema = new Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+export default Contact;
